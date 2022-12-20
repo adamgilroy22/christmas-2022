6d87e906f3c870ebdbe32c30d7f736f77c1fcbd1
Adam Gilroy <adamgilroy22@gmail.com>
2022-12-20 16:26:53 +0000

    Add countdown to christmas

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,32 @@ document.addEventListener('DOMContentLoaded', function() {
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
 });
+
+// Christmas Countdown
+var countDownDate = new Date("Dec 25, 2022 00:00:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the result in the element with id="demo"
+  document.getElementById("xmasCountdown").innerHTML = "There's Only <p>" + days + " days " + hours + " hours<p>"
+  + minutes + " minutes and " + seconds + " seconds <p>Until Christmas!";
+
+  // If the count down is finished, write some text
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("xmasCountdown").innerHTML = "TODAY IS CHRISTMAS!";
+  }
+}, 1000);
